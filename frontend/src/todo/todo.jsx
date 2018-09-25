f07a79e5a200b6586f3243a66c90aaf3fdc5ee06
Henrique Tavares <ihenrits@gmail.com>
2018-09-25 00:19:41 -0300

    Evento onChange

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -7,19 +7,25 @@ export default class Todo extends Component {
 	
 	constructor(props) {
 		super(props)
+		this.state = { description: '', list: [] }
 		this.handleAdd = this.handleAdd.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	}
+
+
+	handleChange(e) {
+		this.setState({...this.state, description: e.target.value})
 	}
 
 	handleAdd() {
-		console.log(this)
-		console.dir(this)
+		alert(this.state.description)
 	}
 
 	render() {
 		return(
 			<div>
 				<PageHeader name='Tarefas' small='Cadastro' />
-				<TodoForm handleAdd={ this.handleAdd } />
+				<TodoForm description={this.state.description} handleChange={this.handleChange} handleAdd={this.handleAdd} />
 				<TodoList />
 			</div>
 		)
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -6,7 +6,8 @@ export default props => (
 	<div role='form' className='todoForm'>
 
 		<Grid cols='12 9 10'>
-			<input id='description' className='form-control' placeholder='Adicione uma terefa'></input>
+			<input id='description' className='form-control' placeholder='Adicione uma terefa' onChange={props.handleChange} 
+				  value={props.description} ></input>
 		</Grid>
 
 		<Grid cols='12 3 2'>
